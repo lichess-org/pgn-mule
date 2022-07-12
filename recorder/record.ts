522d62e972befbc00a1b1e3d2d0b84a6962fc8f8
Thibault Duplessis <t@lichess.org>
2022-07-12 15:24:51 +0200

    subproject to record arbitrary http endpoints to debug sources

---
+++ recorder/record.ts
@@ -0,0 +1,35 @@
+import { mkdir, writeFile } from 'node:fs/promises';
+
+const path = process.argv[2];
+const url = process.argv[3];
+const seconds = parseInt(process.argv[4]);
+let lastContent = '';
+
+async function main() {
+  await mkdir(path, { recursive: true });
+  await fetchPeriodically();
+}
+
+async function fetchPeriodically() {
+  const res = await fetch(url);
+  const content = await res.text();
+  if (content != lastContent) {
+    lastContent = content;
+    const file = `${path}/${dateString()}`;
+    await writeFile(file, content);
+    console.log(`\n\n${file}\n\n`);
+    console.log(content);
+    console.log('----------------------------------------------------------');
+  }
+  await sleep(seconds * 1000);
+  await fetchPeriodically();
+}
+
+const dateString = () => {
+  const d = new Date();
+  return `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}_${d.getHours()}-${d.getMinutes()}-${d.getSeconds()}`;
+};
+
+const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+
+main();
