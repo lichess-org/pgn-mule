38edfa591218deeabe6d6a14624a7c96cf3dc642
kraktus <kraktus@users.noreply.github.com>
2024-01-20 19:04:29 +0100

    Add `jest` for testing

---
+++ src/source/chessCom.test.ts
@@ -0,0 +1,11 @@
+import {describe, expect, test} from '@jest/globals';
+
+function sum(a, b) {
+  return a + b;
+}
+
+describe('sum module', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
